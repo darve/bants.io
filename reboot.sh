5d01e2d3049becac0d05da81dacaca2349036f9e
David Woollard <dave@darve.co.uk>
2015-01-27 08:51:54 +0000

    Updated the folder location in reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PATH=/usr/local/bin:$PATH
-forever start --sourceDir /var/www/darve app.js port=80 >> /darvelog.txt 2>&1
+forever start --sourceDir /var/www/bants.io app.js port=80 >> /darvelog.txt 2>&1
 
